f49a2e87a4ea672d6b599de164942e91ce900c11
phucnguyen1998 <phucnguyen11011998@gmail.com>
2021-08-09 18:17:38 +0700

    design trello front-end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,86 @@
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Helooo world</h1>
+    <div className="trello-master">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://ss-images.saostar.vn/wwebp700/2019/02/02/4544869/ua.jpg" alt="phucng-alt" />
+              Title: Phucng98
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://ss-images.saostar.vn/wwebp700/2019/02/02/4544869/ua.jpg" alt="phucng-alt" />
+              Title: Phucng98
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://ss-images.saostar.vn/wwebp700/2019/02/02/4544869/ua.jpg" alt="phucng-alt" />
+              Title: Phucng98
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://ss-images.saostar.vn/wwebp700/2019/02/02/4544869/ua.jpg" alt="phucng-alt" />
+              Title: Phucng98
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://ss-images.saostar.vn/wwebp700/2019/02/02/4544869/ua.jpg" alt="phucng-alt" />
+              Title: Phucng98
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
